4479d8c64fb0acf42c0de8928cd73b462cfe8e06
dkiriinya <dkiriinya06sc@gmail.com>
2024-01-30 14:45:20 +0300

    learnt how to add child components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
+import About from "./About";
 
 function App() {
   return (
     <div>
       <Article />
       <Comment />
+      <About />
+      
     </div>
   );
 }
